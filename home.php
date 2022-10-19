9b7d6c87021680129be2fb1d6ac75aade3b9e861
iamlovish <108421063+iamlovish@users.noreply.github.com>
2022-10-19 22:09:47 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,20 @@
+<?php session_start();
+if(empty($_SESSION['id'])):
+    header('Location:index.php');
+endif;
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home page</title>
+</head>
+<body>
+
+    <a href="logout process.php"><div style="float:right"><button>logout</button></div></a>
+
+    <h1>Welcome to home page...!</h1>
+
+</body>
+</html>
+          
